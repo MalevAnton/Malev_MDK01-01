7da70bb3405f1c47642da86e977f38ef0576ef9e
Malev <antonio.volk2002@gmail.com>
2023-01-18 16:04:11 +0300

    Добавьте файлы проекта.

---
+++ MalevPractice14/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MalevPractice14
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Model.tbArif = TBArif;
+            Model.tbResult = TBResult;
+            CmbArifmet.ItemsSource = Model.datalist;
+        }
+
+        private void CmbArifmet_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Model.Combo = CmbArifmet.SelectedIndex;
+        }
+
+        private void TboxArif1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Model.a1 = TboxArif1;
+        }
+
+        private void TboxArif2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Model.a2 = TboxArif2;
+        }
+
+        private void BtnClick_Click(object sender, RoutedEventArgs e)
+        {
+            Model.tbResult = TBResult;
+        }
+    }
+}
---
+++ MalevPractice14/Model.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MalevPractice14
+{
+    public static class Model
+    {
+        static int a;
+        static int b;
+        static int result;
+        public static List<string> datalist = new List<string> { "Сложение", "Вычитание", "Умножение", "Деление" };
+
+        public static TextBlock tbResult;
+        public static TextBlock tbArif;
+        public static TextBox a1;
+        public static TextBox a2;
+
+        public static int Combo
+        {
+            set
+            {
+                if (datalist[value] == "Сложение")
+                {
+                    tbArif.Text = "+";
+                }
+
+                else if (datalist[value] == "Вычитание")
+                {
+                    tbArif.Text = "-";
+                }
+
+                else if (datalist[value] == "Умножение")
+                {
+                    tbArif.Text = "*";
+                }
+
+                else if (datalist[value] == "Деление")
+                {
+                    tbArif.Text = ":";
+                }
+            }
+        }
+
+        public static int Result
+        {
+            get
+            {
+                if (tbArif.Text == "Сложение")
+                {
+                    a = Convert.ToInt32(a1.Text);
+                    b = Convert.ToInt32(a2.Text);
+                    result = a + b;
+                    return result;
+                }
+
+                else if (tbArif.Text == "Вычитание")
+                {
+                    a = Convert.ToInt32(a1.Text);
+                    b = Convert.ToInt32(a2.Text);
+                    result = a - b;
+                    return result;
+                }
+
+                else if (tbArif.Text == "Умножение")
+                {
+                    a = Convert.ToInt32(a1.Text);
+                    b = Convert.ToInt32(a2.Text);
+                    result = a * b;
+                    return result;
+                }
+
+                else if (tbArif.Text == "Деление")
+                {
+                    a = Convert.ToInt32(a1.Text);
+                    b = Convert.ToInt32(a2.Text);
+                    result = a / b;
+                    return result;
+                }
+
+                return result;
+            }
+
+            set
+            {
+                result = value;
+                tbResult.Text = result.ToString();
+            }
+        }
+    }
+}
